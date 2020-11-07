72abedb43f1527dc10bbde0fbac39ed4a372808c
bencouser <73387835+bencouser@users.noreply.github.com>
2020-11-07 17:16:07 +0000

    Update 4.py
    final solution

--- 4.py
+++ 4.py
@@ -8,20 +8,17 @@
 def digits(number):
         digit = [int(x) for x in str(number)]
         return digit
-        
+
 def is_it_palindromic(number):
         digit = digits(number)
-        originalDigits = digit
-        digit.reverse()
-        reverseDigits = digit
-        return originalDigits == reverseDigits
+        return digit == digit[::-1]
 
 largest_palindromic = 0
 
-for i in range(101, 1000):
-        for j in range(101, 1000):
+for i in range(100, 999):
+        for j in range(i+1, 1000):
                 possible_pal = i * j
-                if is_it_palindromic(possible_pal) == True:
+                if is_it_palindromic(possible_pal) and possible_pal > largest_palindromic:
                         largest_palindromic = possible_pal
 
 print(largest_palindromic)
