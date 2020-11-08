9834769e849eee04cd6b9999bd0d26b3c6dca1b9
bencouser <73387835+bencouser@users.noreply.github.com>
2020-11-08 12:51:35 +0000

    Update 7.py
    final solution

--- 7.py
+++ 7.py
@@ -4,7 +4,7 @@
 
 def all_factors(N):
         factors = []
-        for i in range(1, int(np.sqrt(N) + 1)): #we only need to check up to the root of ur number
+        for i in range(1, int(np.sqrt(N) + 1)):
                 if N % i == 0:
                         factors.append(i)
         factors.append(N)
@@ -12,3 +12,18 @@ def all_factors(N):
 
 def is_it_prime(n):
         return len(all_factors(n)) == 2
+
+primes = []
+number = 2
+
+def finding_primes(n):
+        while (len(primes) < 10002):
+                if is_it_prime(n):      
+                        primes.append(n)
+                        n += 1
+                else:
+                        n += 1
+
+finding_primes(number)
+
+print(primes[10000])
