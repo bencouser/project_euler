c97c7f9679ebd9e37eb9d50ef4ae860cb7052e7b
bencouser <73387835+bencouser@users.noreply.github.com>
2020-11-19 18:51:39 +0000

    Add files via upload
    final sol

--- 15.py
+++ 15.py
@@ -4,10 +4,11 @@
 
 # note: in 2x2 the length of the route is always 4 as we need to move 2 right and 2 bottom regardless
 #       there are 3*2 routes 3!
+#       one constraint is that line has to be (if box is n x n) 2n units long
+#       in example there are mirror ones
+#       try 3x3
 
-x = 1
-
-for i in range(1,20):
-    x *= i
-
-print(x)
+import math
+def central_binomals(n):
+    return (math.factorial(2*n))/(math.factorial(n))**2
+print(central_binomals(20))
