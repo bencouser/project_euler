6dd9356c07849325714b422397555713af0ade07
bencouser <73387835+bencouser@users.noreply.github.com>
2020-11-19 12:03:50 +0000

    Add files via upload
    final sol, assumed it would be odd, originally was using zero which was infinite

--- 14.py
+++ 14.py
@@ -7,6 +7,10 @@
 
 # find the longest sequence starting with a number under 1000000
 
+# powers of 2 will be fastest route down
+
+# we dont need to append first and last terms as they are in every list, also i assume it would be odd
+
 def even(number):
     newNumber = number / 2
     return newNumber
@@ -16,19 +20,27 @@ def odd(number):
     newNumber = (3 * number) + 1
     return newNumber
 
-term = 13
-terms = []
-terms.append(term)
+largest_term = 0
+largest_terms = []
 
-while term != 1:
-    if term % 2 == 0:
-        newTerm = even(term)
-        terms.append(newTerm)
-        term = newTerm
-    else:
-        newTerm = odd(term)
-        terms.append(newTerm)
-        term = newTerm
+for half_term in range(499999):
+    term = (half_term * 2) + 1
+    terms = []
+    original_term = term
+    while term != 1:
+        if term % 2 == 0:
+            newTerm = even(term)
+            terms.append(newTerm)
+            term = newTerm
+        else:
+            newTerm = odd(term)
+            terms.append(newTerm)
+            term = newTerm
+    if len(terms) > len(largest_terms):
+        largest_terms = terms
+        largest_term = original_term
 
-print(terms, len(terms))
+
+
+print(largest_term)
 
