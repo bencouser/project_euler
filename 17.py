355bb13f1ca55810634504011347d5a264bf6b0b
bencouser <73387835+bencouser@users.noreply.github.com>
2020-11-24 15:33:11 +0000

    Add files via upload
    took longer than expected that one, clearly need to get better working with strings

--- 17.py
+++ 17.py
@@ -2,18 +2,19 @@ small = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "ni
 
 tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 
-hundredAnd = 10
-
-N = 10
+def words(n):
+    if 0 <= n < 20:
+        return small[n]
+    elif 20 <= n < 100:
+        return tens[n // 10] + (small[n % 10] if (n % 10 != 0) else "")
+    elif 100 <= n < 1000:
+        return small[n // 100] + "hundred" + (("and" + words(n % 100)) if (n % 100 != 0) else "")
+    elif n == 1000:
+        return "onethousand"
 
 sum = 0
-
-for i in range(1, N+1):
-    if i < 10:
-        number = small[i].split(",")
-        sum += len(number)
-
-    else:
-        pass
+for i in range(1, 1001):
+    sum += len(words(i))
 
 print(sum)
+
