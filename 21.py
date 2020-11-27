679a21bcbf9fe6f51355145272a0615a139bab01
bencouser <73387835+bencouser@users.noreply.github.com>
2020-11-27 12:20:49 +0000

    Add files via upload
    fed up of having a bad factors function so i rewrote it, only the start of the question, defd factors and d

---
+++ 21.py
@@ -0,0 +1,31 @@
+# d(n) is the sum of divisors of n
+# if d(a) = b and d(b) = a, where a != b
+# this is an amicable pair
+# d(220) = 284 and d(284) = 220 for example
+
+# evaluate the sum of all amicable numbers under 10000.
+import math
+
+def factors(number):
+    half_factors = []
+    factors = []
+    for potentialFactor in range(1, int(math.sqrt(number)) + 1):
+        if number % potentialFactor == 0:
+            half_factors.append(potentialFactor)
+    size = len(half_factors)
+    for i in range(size):
+        factors.append(half_factors[i])
+    for factor in range(size):
+        fact = number / half_factors[factor]
+        if fact in factors:
+            pass
+        else:
+            factors.append(fact)
+    return factors
+
+
+def d(number):
+    return sum(factors(number)) - number
+
+print(d(220))
+
