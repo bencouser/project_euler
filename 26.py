6989f9a3cb1d58a89e580883749ad91f6bd1d039
bencouser <73387835+bencouser@users.noreply.github.com>
2020-12-10 09:52:59 +0000

    Add files via upload
    thinking it must be a prime number, guessed largest prime but I was wrong

---
+++ 26.py
@@ -0,0 +1,38 @@
+# find the value of d < 1000 for which 1/d contains the longest recurring cycle in its
+# decimal fraction part
+
+# % for remainder, // for devision without remain
+
+#function to check length of a remainder 
+
+# in example 7 is largest and is prime, lets find the primes from 1-1000
+
+import math
+
+primeList = []
+
+def getFactors(number):
+    half_factors = []
+    factors = []
+    for potentialFactor in range(1, int(math.sqrt(number)) + 1):
+        if number % potentialFactor == 0:
+            half_factors.append(potentialFactor)
+    size = len(half_factors)
+    for i in range(size):
+        factors.append(half_factors[i])
+    for factor in range(size):
+        fact = number / half_factors[factor]
+        if fact in factors:
+            pass
+        else:
+            factors.append(fact)
+    return factors
+
+def isitPrime(number):
+    return len(getFactors(number)) == 2    
+
+for i in range(1,1001):
+    if isitPrime(i):
+        primeList.append(i)
+
+print(max(primeList))
