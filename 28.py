6cbc59549bcfcd3cccbca2173ffa53813d054b44
bencouser <73387835+bencouser@users.noreply.github.com>
2020-12-11 11:56:32 +0000

    Add files via upload
    thoughts on how to solve. not implemented yet

---
+++ 28.py
@@ -0,0 +1,13 @@
+# starting at 1 and moving right and out in a clockwise direction gives the form #seewebsite
+# the sum of the diagonals is 101
+
+# what is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed by the same
+# way
+
+# top right corner of square is always the dimension of the square, squared
+# 1 = 1**2, 9 = 3**2, 25 = 5**2
+
+sum = 1
+
+for i in range(1, 26):
+
