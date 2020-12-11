0a89d31a9fa9b4ee10d85b464242f631a4c02470
bencouser <73387835+bencouser@users.noreply.github.com>
2020-12-11 20:17:57 +0000

    Add files via upload

--- 30.py
+++ 30.py
@@ -1,15 +1,18 @@
 # find the sum of all the numbers that can be written as the sum of fifth powers of their digits
 
+# in example case the solutions only went to 4 digits so i will guess for 5th powers
+# they will go up to 5 digit (didnt work)s
+
 def digits(number):
     digit = [int(x) for x in str(number)]
     return digit
 
 fourPowerNumbers = []
 
-for i in range(2,10001):
+for i in range(2,100001):
     numberDigits = digits(i)
     powered = []
-    powered = [x**4 for x in numberDigits]
+    powered = [x**5 for x in numberDigits]
     if sum(powered) == i:
         fourPowerNumbers.append(i)
 
