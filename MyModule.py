b2bff0fcb2fdc0ef185f8d21ae40607b10f10134
bencouser <73387835+bencouser@users.noreply.github.com>
2020-12-14 08:36:51 +0000

    Add files via upload
    a module that I will start using from q 31 to speed some things up. Upload to site-packages if you wish to use it.

---
+++ MyModule.py
@@ -0,0 +1,41 @@
+import math
+
+
+# Function takes an int
+# Returns a list of the ints factors including itself
+def get_factors(number):
+    half_factors = []
+    factors = []
+    for potentialFactor in range(1, int(math.sqrt(abs(number))) + 1):
+        if number % potentialFactor == 0:
+            half_factors.append(potentialFactor)
+    size = len(half_factors)
+    for i in range(size):
+        factors.append(float(half_factors[i]))
+    for factor in range(size):
+        fact = number / half_factors[factor]
+        if fact in factors:
+            pass
+        else:
+            factors.append(fact)
+    return factors
+
+
+# Function takes int argument
+# Returns True if the int is prime, False if not
+def is_it_prime(number):
+    return len(get_factors(number)) == 2    
+
+
+# Function takes an int, n digits long
+# Returns a list of length n of all its digits
+def find_digits(number):
+    digits = [int(x) for x in str(number)]
+    return digits
+
+
+# Function takes a positive integer
+# Returns True if palindromic
+def is_it_palindromic(number):
+    digit = find_digits(number)
+    return digit == digit[::-1]
