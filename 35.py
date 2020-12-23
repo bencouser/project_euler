10ec2c3330e8d6e88b9c832565145d0623e53901
bencouser <73387835+bencouser@users.noreply.github.com>
2020-12-23 17:54:20 +0000

    Add files via upload
    That one was a lot more work than i would have anticipated. final sol done

--- 35.py
+++ 35.py
@@ -2,7 +2,6 @@
 # How many circular primes are there below one million
 
 import MyModule as mm
-import itertools
 
 count = 0
 listPrimes = []
@@ -11,16 +10,26 @@ def digit_to_number(digits):
     s = ''.join(map(str, digits))
     return int(s)
 
+def find_circular_perms(digits):
+    all_perms = []
+    all_perms.append(digits)
+    for i in range(len(digits)-1):
+        new_perm = digits[i+1:] + digits[:i+1]
+        all_perms.append(new_perm)
+    return all_perms
+
 for i in range(2, 1000001):
-    if mm.is_it_prime(i) == True:
+    if mm.is_it_prime(i):
         listPrimes.append(i)
 
 for prime in listPrimes:
     digits = mm.find_digits(prime)
-    all_perms = itertools.permutations(digits)
+    miniCount = 1
+    all_perms = find_circular_perms(digits)
     for perm in all_perms:
-        if mm.is_it_prime(digit_to_number(perm)) == False:
-            break
-    
+        if mm.is_it_prime(digit_to_number(perm)) is False:
+            miniCount = 0
+    if miniCount == 1:
+        count += 1
 
 print(count)
