752365461b7325961e52eb50641d4283ecbf7247
bencouser <73387835+bencouser@users.noreply.github.com>
2021-01-03 15:27:05 +0000

    Add files via upload
    pretty gutted with that one as i had to look for a solution as i didnt know what was going wrong. no idea why my pandigital checker didn't work but once i switched it for another i found it worked

--- 32.py
+++ 32.py
@@ -5,12 +5,25 @@
 # Find the sum of all products whose m/m/p identity can be written as 1 => 9 pandigital
 # Note: Some products can be obtained in more than one way
 
-import MyModule as mm
+import math
 
-def is_it_pandigital(number):
-    digits = mm.find_digits(number)
-    return len(set(str(number))) == len(digits)
-    
-print(is_it_pandigital(123))
-print(is_it_pandigital(44123))
+def is_it_pandigital(listDigits):
+    return "".join(sorted(listDigits)) == "123456789"
 
+def find_pandigital_product(number):
+    for i in range(1, int(math.sqrt(number)) + 1):
+        if number % i == 0:
+            possible_pp = str(number) + str(i) + str(number // i)
+            print(possible_pp)
+            if is_it_pandigital(possible_pp):
+                print(possible_pp)
+                return True
+    return False
+
+sum = 0
+
+for j in range(10, 10001):
+    if find_pandigital_product(j):
+        sum += j
+        
+print(sum)
