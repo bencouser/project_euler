851c8e254bd27a6e3258d50d461d8ecaef919170
bencouser <73387835+bencouser@users.noreply.github.com>
2021-01-14 20:40:48 +0000

    Add files via upload
    made fast by first checking the first and last digits are prime. run out of time, will continue tomorrow

--- 37.py
+++ 37.py
@@ -5,13 +5,16 @@ import MyModule as mm
 
 countTruncatableBoth = 0
 sumTruncatableBoth = 0
-counter = 13
+number = 13
 
 while countTruncatableBoth < 12:
-    if is_it_prime(counter):
-        digits = mm.find_digits(counter)
-        for digit in range(len(digits) - 1):
-            
+    if mm.is_it_prime(number):
+        digits = mm.find_digits(number)
+        length = len(digits)
+        if is_it_prime(digits[0]) and is_it_prime(digits[length - 1]):
+            for i in range(length):
+                
+    number += 1
 
         
 
